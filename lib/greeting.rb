f9f44247648d20231560d1cf550054c50d6821af
Andy Leavitt <andyleavitt@gmail.com>
2023-08-10 16:58:54 -0600

    refactored towards a recursive approach. Tests passing through req 5.

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,5 +1,48 @@
 class Greeting
-  def self.greet(name)
 
+  def greet(name = "my friend")
+    if input_is_list?(name)
+      greet_many(name)
+    else
+      shouting_at?(name) ? shout_one(name) : greet_one(name)
+    end
+  end
+
+  private
+
+  def input_is_list?(name)
+    name.kind_of?(Array)
+  end
+
+  def shouting_at?(name)
+    name == name.upcase
+  end
+
+  def shout_one(name)
+    "HELLO #{name}!"
+  end
+
+  def greet_one(name)
+    "Hello, #{name}."
+  end
+
+  def compose_names(name_list, greeting, oxford_comma)
+    name, *rest = name_list
+    if (name_list.size == 1)
+      return "#{greeting}#{oxford_comma ? "," : ""} and #{name}."
+    end
+
+    greeting += ", #{name}"
+
+    if oxford_comma && name_list.size==1
+      greeting+=","
+    end
+
+    compose_names(rest, greeting, oxford_comma)
+  end
+
+  def greet_many(name_list)
+    oxford_comma = name_list.size !=2
+    compose_names(name_list, "Hello", oxford_comma)
   end
 end
--- spec/greeting_spec.rb
+++ spec/greeting_spec.rb
@@ -4,7 +4,23 @@ require_relative '../lib/greeting'
 RSpec.describe Greeting do
   describe "#greet" do
     it "can greet a friend" do
-      expect(Greeting.greet("Bob")).to eq "Hello, Bob."
+      expect(subject.greet("Bob")).to eq "Hello, Bob."
+    end
+
+    it "can greet a nameless friend" do
+      expect(subject.greet).to eq "Hello, my friend."
+    end
+
+    it "can shout at a friend" do
+      expect(subject.greet("JERRY")).to eq "HELLO JERRY!"
+    end
+
+    it "can greet two friends" do
+      expect(subject.greet(["Jill", "Jane"])).to eq "Hello, Jill and Jane."
+    end
+
+    it "can greet more than two friends" do
+      expect(subject.greet(["Amy", "Brian", "Charlotte"])).to eq "Hello, Amy, Brian, and Charlotte."
     end
   end
 end
